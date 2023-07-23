41c296509a5874aaffd45ae5c07df12af6a845ae
arklual <arklual1@gmail.com>
2023-07-23 08:12:56 +0300

    update

--- bot.py
+++ bot.py
@@ -13,6 +13,9 @@ e = Executor(dp)
 
 @dp.message_handler(lambda m: m.text.startswith('/start'))
 async def start(message: types.Message):
+    if message.text == '/start':
+        await message.answer('Чтобы включить бота введите: "/start название_бота"\nЧтобы выключить бота: "/stop название_бота"\nЧтобы посмотреть статус бота: "/status название_бота"')
+        return
     bot_to_start = message.text.split(' ')[-1]
     os.system(f'systemctl start {bot_to_start}')
     await message.answer(f'{bot_to_start} запущен')
